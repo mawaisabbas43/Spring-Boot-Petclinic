883056d696bedf4a6ea1792739c9c2d450786878
Muhammad Awais <muhammad.awais1@pk.nestle.com.pk>
2021-12-12 03:56:31 +0500

    Refactored for multi-module maven project.

--- pet-clinic-web/src/main/java/com/petclinic/springbootpetclinic/SpringBootPetclinicApplication.java
+++ pet-clinic-web/src/main/java/com/petclinic/springbootpetclinic/SpringBootPetclinicApplication.java
@@ -2,12 +2,9 @@ package com.petclinic.springbootpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootPetclinicApplication {
-
-    private static ApplicationContext ctx;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootPetclinicApplication.class, args);
