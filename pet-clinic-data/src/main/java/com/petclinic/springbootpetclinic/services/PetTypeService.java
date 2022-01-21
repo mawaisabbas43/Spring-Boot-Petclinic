5d8d5885e41b6f4e2e65184dd9f601746232f162
Muhammad Awais <muhammad.awais1@pk.nestle.com.pk>
2022-01-22 03:10:27 +0500

    Added Pet Type Map Service Impl. Closes #20

---
+++ pet-clinic-data/src/main/java/com/petclinic/springbootpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.petclinic.springbootpetclinic.services;
+
+import com.petclinic.springbootpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
