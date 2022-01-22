4b233b8db61530b653e9cd916b055aa0c4da5d22
Muhammad Awais <muhammad.awais1@pk.nestle.com.pk>
2022-01-23 01:55:15 +0500

    Added Specialties Map Service Impl. Closes #21

---
+++ pet-clinic-data/src/main/java/com/petclinic/springbootpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,8 @@
+package com.petclinic.springbootpetclinic.services;
+
+import com.petclinic.springbootpetclinic.model.Speciality;
+import com.petclinic.springbootpetclinic.model.Vet;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+
+}
