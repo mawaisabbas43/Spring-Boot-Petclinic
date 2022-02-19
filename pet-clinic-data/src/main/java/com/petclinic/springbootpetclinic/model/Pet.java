4aa60c8ac369ff42fe1d98b7281bdeddc83f58de
Muhammad Awais <muhammad.awais1@pk.nestle.com.pk>
2022-02-19 18:48:31 +0500

    updated Visit to jpa Entity . Closes #29

--- pet-clinic-data/src/main/java/com/petclinic/springbootpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/petclinic/springbootpetclinic/model/Pet.java
@@ -1,10 +1,9 @@
 package com.petclinic.springbootpetclinic.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Pet extends BaseEntity {
@@ -16,6 +15,9 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "owner_id")
     private Owner owner;
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
+    private Set<Visit> visits=new HashSet<>();
 
     public void setPetType(PetType petType) {
         this.petType = petType;
@@ -47,5 +49,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
