0577433d43b348566e5cfae8794bb2410f95e9be
Muhammad Awais <muhammad.awais1@pk.nestle.com.pk>
2022-02-20 01:48:52 +0500

    Added Spring Data JPA Service for Owners. Closes #31

--- pet-clinic-data/src/main/java/com/petclinic/springbootpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/petclinic/springbootpetclinic/repositories/OwnerRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
