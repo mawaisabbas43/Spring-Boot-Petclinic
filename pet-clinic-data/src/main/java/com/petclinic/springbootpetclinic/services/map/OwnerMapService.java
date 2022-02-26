ca85dbd395ce15ac7595a534ebbdb8d356153895
Muhammad Awais <muhammad.awais1@pk.nestle.com.pk>
2022-02-27 03:33:03 +0500

    Added Test for Owner Map Service. Closes #45

--- pet-clinic-data/src/main/java/com/petclinic/springbootpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/petclinic/springbootpetclinic/services/map/OwnerMapService.java
@@ -70,6 +70,10 @@ public class OwnerMapService
 
     @Override
     public Owner findByLastName(String lastName) {
-        return null;
+        return super.findAll()
+                .stream()
+                .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
+                .findFirst()
+                .orElse(null);
     }
 }
